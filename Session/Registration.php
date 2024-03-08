05751f53b0da1326e294828d8f00404852a205ee
MD.-ALAMIN-HOSSEN <99692491+MD-ALAMIN-HOSSEN@users.noreply.github.com>
2024-03-08 10:47:25 +0600

    Add files via upload

---
+++ Session/Registration.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registration Form</title>
+  <style>
+    header, body, footer {
+      border: 1px solid #ddd; 
+      padding: 10px; 
+      margin-bottom: 10px; 
+    }
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+    }
+  </style>
+</head>
+<header >
+    <div class="flex-container">
+    <h1>X Company</h1>
+    <nav>
+      <a href="publicHome.php">Home</a>
+      <a href="Login.php">Login</a>
+      <a href="Registration.php">Registration</a>
+    </nav>
+    </div>
+  </header>
+<body>
+<fieldset>
+ <legend>REGISTRATION</legend>   
+      <form action=".php" method="post">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" name="name" id="name" required>
+        
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" name="email" id="email" required>
+        </div>
+        <div class="form-group">
+          <label for="username">Username:</label>
+          <input type="text" name="username" id="username" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" name="password" id="password" required>
+        </div>
+        <div class="form-group">
+          <label for="confirm_password">Confirm Password:</label>
+          <input type="password" name="confirm_password" id="confirm_password" required>
+        </div>
+        <div class="form-group">
+          <label for="gender">Gender:</label>
+          <input type="radio" name="gender" id="male" value="male" required> Male
+          <input type="radio" name="gender" id="female" value="female" required> Female
+          <input type="radio" name="gender" id="other" value="other" required> Other
+        </div>
+        <div class="form-group">
+          <label for="date_of_birth">Date of Birth:</label>
+          <input type="date" name="date_of_birth" id="date_of_birth" required>
+        </div>
+        <div class="form-group">
+          <button type="submit">Submit</button>
+          <button type="reset">Reset</button>
+        </div>
+      </form>
+    
+  
+  </fieldset>
+</body>
+<footer>
+    <p>Copyright © 2017</p>
+  </footer>
+</html>
---
+++ Session/viewProfile.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoggedInDashboard</title>
+    <style>
+    header,
+    body,
+    footer {
+        border: 1px solid #ddd;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    .div-main {
+        border: 1px solid #ddd;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
+
+    .flex-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .flex-container-main {
+        display: flex;
+
+    }
+
+    .flex-container-main-nav {
+        display: flex;
+        justify-content: flex-start;
+        padding: 20px;
+    }
+    </style>
+</head>
+
+<header>
+    <div class="flex-container">
+        <h1>X Company</h1>
+        <nav>
+            Logged in as
+            <a href="#">Bob</a> |
+            <a href="Logout.php">Logout</a>
+        </nav>
+    </div>
+</header>
+
+<body>
+    <div class="flex-container-main">
+        <div class="flex-container-main-nav div-main">
+            <nav>
+                <b>Account</b>
+                <hr>
+                <ul>
+                    <li><a href="LoggedInDashboard.php">Dashboard</a></li>
+                    <li><a href="viewProfile.php">View Profile</a></li>
+                    <li><a href="EditProfile.php">Edit Profile</a></li>
+                    <li><a href="profilePicture.php">Change Profile Picture</a></li>
+                    <li><a href="changePassword.php">Change Password</a></li>
+                    <li><a href="Logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="account-details div-main">
+            <fieldset>
+                <legend><h2>PROFILE</h2></legend>
+                <form action="" method="">
+                   Name         :Bob <br>
+                   Email        :bob@aiub.edu <br>
+                   Gender       :Male <br>
+                   Date of Birth:19/09/1998
+                    <a href="EditProfile.php">Edit Profile</a><br>
+                    <a href="profilePicture.php">Change</a>
+                </form>
+            </fieldset>
+        </div>
+    </div>
+
+</body>
+<footer>
+    <p>Copyright © 2017</p>
+</footer>
+
+</html>
